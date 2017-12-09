dc32debe1616d338b07dff78d5456a769668659b
Paul <paulwb@gmail.com>
2017-12-09 15:51:45 -0800

    Switch to FastLED
    testing color order, correct setting is GBR

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,32 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
-#define PIN 12
+#define CORRECTION TypicalLEDStrip
+#define LED_PIN 12
+#define NUM_LEDS 16
+#define BRIGHTNESS 255
+#define LED_TYPE WS2811
+#define COLOR_ORDER GBR
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[NUM_LEDS];
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait)
-{
-  for (uint16_t i = 0; i < strip.numPixels(); i++)
-  {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos)
-{
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85)
-  {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  else if (WheelPos < 170)
-  {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  else
-  {
-    WheelPos -= 170;
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}
+CRGB color = 0x601040;
 
 void setup()
 {
-  strip.begin();
-  //  strip.setBrightness(64);
-  strip.show(); // Initialize all pixels to 'off'
+
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+      .setCorrection(CORRECTION);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+  FastLED.show();
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait)
+void loop()
 {
-  uint16_t i, j;
 
-  for (j = 0; j < 256 * 5; j++)
-  { // 5 cycles of all colors on wheel
-    for (i = 0; i < strip.numPixels(); i++)
-    {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
+  leds[0] = CRGB(255, 0, 0);
+  leds[1] = CRGB(0, 255, 0);
+  leds[2] = CRGB(0, 0, 255);
+  FastLED.show();
+  FastLED.delay(1000);
 }
-
-void loop() { rainbowCycle(20); }
