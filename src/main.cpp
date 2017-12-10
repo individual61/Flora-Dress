0c0fc5845d7fafa61da5f16346e15ad5064c7977
Paul <paulwb@gmail.com>
2017-12-09 16:11:09 -0800

    Flash each LED white in turn for 1 second

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 
 #define CORRECTION TypicalLEDStrip
 #define LED_PIN 12
+#define BACKLED_PIN 8
 #define NUM_LEDS 16
+#define NUM_BACKLEDS 1
 #define BRIGHTNESS 255
 #define LED_TYPE WS2811
 #define COLOR_ORDER GBR
 
 CRGB leds[NUM_LEDS];
+CRGB backled[1];
 
 CRGB color = 0x601040;
 
@@ -19,14 +22,29 @@ void setup()
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.clear();
   FastLED.show();
-}
 
+  FastLED.addLeds<LED_TYPE, BACKLED_PIN, COLOR_ORDER>(backled, NUM_BACKLEDS)
+      .setCorrection(CORRECTION);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear();
+
+  backled[0] = color;
+
+  FastLED.show();
+}
+int index = 0;
 void loop()
 {
+  index++;
+  if (index == NUM_LEDS)
+  {
+    index = 0;
+  };
 
-  leds[0] = CRGB(255, 0, 0);
-  leds[1] = CRGB(0, 255, 0);
-  leds[2] = CRGB(0, 0, 255);
+  leds[index] = CRGB(255, 255, 255);
+
   FastLED.show();
   FastLED.delay(1000);
+  leds[index] = CRGB(0, 0, 0);
+  FastLED.show();
 }
